b85f25340b7787d3eb447ad82244ce0429c4ba08
Sunny Patel <sunnypatel2048@gmail.com>
2023-10-25 23:56:40 +0530

    Added password hashing

--- src/main/java/com/sunny/expensetracker/repositories/UserRepositoryImpl.java
+++ src/main/java/com/sunny/expensetracker/repositories/UserRepositoryImpl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -34,6 +35,7 @@ public class UserRepositoryImpl implements UserRepository {
 	
 	@Override
 	public Integer create(String firstName, String lastName, String email, String password) throws EtAuthException {
+		String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(10));
 		try {
 			KeyHolder keyHolder = new GeneratedKeyHolder();
 			jdbcTemplate.update(connection -> {
@@ -41,7 +43,7 @@ public class UserRepositoryImpl implements UserRepository {
 				ps.setString(1,  firstName);
 				ps.setString(2, lastName);
 				ps.setString(3, email);
-				ps.setString(4, password);
+				ps.setString(4, hashedPassword);
 				return ps;
 			}, keyHolder);
 			return (Integer) keyHolder.getKeys().get("USER_ID");
@@ -54,7 +56,7 @@ public class UserRepositoryImpl implements UserRepository {
 	public User findByEmailAndPassword(String email, String password) throws EtAuthException {
 		try {
 			User user = jdbcTemplate.queryForObject(SQL_FIND_BY_EMAIL, userRowMapper, email);
-			if (!password.equals(user.getPassword()))
+			if (!BCrypt.checkpw(password, user.getPassword()))
 				throw new EtAuthException("Invalid email/password");
 			return user;
 		} catch (EmptyResultDataAccessException e) {
