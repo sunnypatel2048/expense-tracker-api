8e5db29d92ea2c63318bb17c9ac00ea96b2fc467
Sunny Patel <sunnypatel2048@gmail.com>
2023-10-26 02:11:48 +0530

    Added jwt token creation and validation

---
+++ src/main/java/com/sunny/expensetracker/Constants.java
@@ -0,0 +1,8 @@
+package com.sunny.expensetracker;
+
+public class Constants {
+	
+	public static final String API_SECRET_KEY = "PUWo7NAXguu6fxt3XPP3mMrxGG0aHhJr";
+	
+	public static final long TOKEN_VALIDITY = 2 * 60 * 60 * (long) 1000;
+}
