c34bcf6c29326e417df5259439e686f26db1463d
Sunny Patel <sunnypatel2048@gmail.com>
2023-10-27 21:55:37 +0530

    Added Repository, Service and addCategory endpoint

---
+++ src/main/java/com/sunny/expensetracker/repositories/CategoryRepository.java
@@ -0,0 +1,20 @@
+package com.sunny.expensetracker.repositories;
+
+import java.util.List;
+
+import com.sunny.expensetracker.domain.Category;
+import com.sunny.expensetracker.exceptions.EtBadRequestException;
+import com.sunny.expensetracker.exceptions.EtResourceNotFoundException;
+
+public interface CategoryRepository {
+
+	List<Category> finaAll(Integer userId) throws EtResourceNotFoundException;
+	
+	Category findById(Integer userId, Integer categoryId) throws EtResourceNotFoundException;
+	
+	Integer create(Integer userId, String title, String description) throws EtBadRequestException;
+	
+	void update(Integer userId, Integer categoryId, Category category) throws EtBadRequestException;
+	
+	void removeById(Integer userId, Integer categoryId);
+}
