3c6036f1ca815d167327a30720d80d91c6f18c6e
Sunny Patel <sunnypatel2048@gmail.com>
2023-10-28 22:07:13 +0530

    Created transaction endpoints

--- src/main/java/com/sunny/expensetracker/repositories/TransactionRepositoryImpl.java
+++ src/main/java/com/sunny/expensetracker/repositories/TransactionRepositoryImpl.java
@@ -1,9 +1,14 @@
 package com.sunny.expensetracker.repositories;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.sunny.expensetracker.domain.Transaction;
@@ -13,34 +18,66 @@ import com.sunny.expensetracker.exceptions.EtResourceNotFoundException;
 @Repository
 public class TransactionRepositoryImpl implements TransactionRepository {
 	
+	private static final String SQL_FIND_ALL = "SELECT TRANSACTION_ID, CATEGORY_ID, USER_ID, AMOUNT, NOTE, TRANSACTION_DATE "
+			+ "FROM ET_TRANSACTIONS WHERE USER_ID = ? AND CATEGORY_ID = ?";
+	
+	private static final String SQL_FIND_BY_ID = "SELECT TRANSACTION_ID, CATEGORY_ID, USER_ID, AMOUNT, NOTE, TRANSACTION_DATE "
+			+ "FROM ET_TRANSACTIONS WHERE USER_ID = ? AND CATEGORY_ID = ? AND TRANSACTION_ID = ?";
+	
+	private static final String SQL_CREATE = "INSERT INTO ET_TRANSACTIONS (TRANSACTION_ID, CATEGORY_ID, USER_ID, AMOUNT, "
+			+ "NOTE, TRANSACTION_DATE) VALUES (NEXTVAL('ET_TRANSACTIONS_SEQ'), ?, ?, ?, ?, ?)";
+	
+	private static final String SQL_UPDATE = "UPDATE ET_TRANSACTIONS SET AMOUNT = ?, NOTE = ?, TRANSACTION_DATE = ? "
+			+ "WHERE USER_ID = ? AND CATEGORY_ID = ? AND TRANSACTION_ID = ?";
+	
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
 	@Override
 	public List<Transaction> findAll(Integer userId, Integer categoryId) {
-		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.query(SQL_FIND_ALL, transactionRowMapper, new Object[] {userId, categoryId});
 	}
 
 	@Override
 	public Transaction findById(Integer userId, Integer categoryId, Integer transactionId)
 			throws EtResourceNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return jdbcTemplate.queryForObject(SQL_FIND_BY_ID, transactionRowMapper, new Object[] {userId, categoryId, transactionId});
+		} catch (Exception e) {
+			throw new EtBadRequestException("Category not found");
+		}
 	}
 
 	@Override
 	public Integer create(Integer userId, Integer categoryId, Double amount, String note, Long transactionDate)
 			throws EtBadRequestException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			KeyHolder keyHolder = new GeneratedKeyHolder();
+			jdbcTemplate.update(connection -> {
+				PreparedStatement ps = connection.prepareStatement(SQL_CREATE, Statement.RETURN_GENERATED_KEYS);
+				ps.setInt(1, categoryId);
+				ps.setInt(2, userId);
+				ps.setDouble(3, amount);
+				ps.setString(4, note);
+				ps.setLong(5, transactionDate);
+				return ps;
+			}, keyHolder);
+			return (Integer) keyHolder.getKeys().get("TRANSACTION_ID");
+		} catch (Exception e) {
+			throw new EtBadRequestException("Invalid request");
+		}
 	}
 
 	@Override
 	public void update(Integer userId, Integer categoryId, Integer transactionId, Transaction transaction)
 			throws EtBadRequestException {
-		// TODO Auto-generated method stub
-
+		try {
+			jdbcTemplate.update(SQL_UPDATE, new Object[] {transaction.getAmount(), transaction.getNote(), 
+															transaction.getTransactionDate(), userId, 
+															categoryId, transactionId});
+		} catch (Exception e) {
+			throw new EtBadRequestException("Invalid request");
+		}
 	}
 
 	@Override
@@ -50,4 +87,12 @@ public class TransactionRepositoryImpl implements TransactionRepository {
 
 	}
 
+	private RowMapper<Transaction> transactionRowMapper = ((rs, rowNum) -> {
+		return new Transaction (rs.getInt("TRANSACTION_ID"),
+				rs.getInt("CATEGORY_ID"),
+				rs.getInt("USER_ID"),
+				rs.getDouble("AMOUNT"),
+				rs.getString("NOTE"),
+				rs.getLong("TRANSACTION_DATE"));
+	});
 }
---
+++ src/main/java/com/sunny/expensetracker/resources/TransactionResource.java
@@ -0,0 +1,70 @@
+package com.sunny.expensetracker.resources;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sunny.expensetracker.domain.Transaction;
+import com.sunny.expensetracker.services.TransactionService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/categories/{categoryId}/transactions")
+public class TransactionResource {
+
+	@Autowired
+	TransactionService transactionService;
+	
+	@GetMapping("")
+	public ResponseEntity<List<Transaction>> getAllTransactions(HttpServletRequest request, 
+												@PathVariable("categoryId") Integer categoryId) {
+		int userId = (Integer) request.getAttribute("userId");
+		List<Transaction> transactions = transactionService.fetchAllTransactions(userId, categoryId);
+		return new ResponseEntity<>(transactions, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{transactionId}")
+	public ResponseEntity<Transaction> getTransactionById(HttpServletRequest request,
+															@PathVariable("categoryId") Integer categoryId,
+															@PathVariable("transactionId") Integer transactionId) {
+		int userId = (Integer) request.getAttribute("userId");
+		Transaction transaction = transactionService.fetchTransactionById(userId, categoryId, transactionId);
+		return new ResponseEntity<>(transaction, HttpStatus.OK);
+	}
+	
+	@PostMapping("")
+	public ResponseEntity<Transaction> addTransaction(HttpServletRequest request, 
+														@PathVariable("categoryId") Integer categoryId,
+														@RequestBody Map<String, Object> transactionMap) {
+		int userId = (Integer) request.getAttribute("userId");
+		double amount = Double.parseDouble(transactionMap.get("amount").toString());
+		String note = (String) transactionMap.get("note");
+		long transactionDate = Long.parseLong(transactionMap.get("transactionDate").toString());
+		Transaction transaction = transactionService.addTransaction(userId, categoryId, amount, note, transactionDate);
+		return new ResponseEntity<>(transaction, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{transactionId}")
+	public ResponseEntity<Map<String, Boolean>> updateTransaction(HttpServletRequest request,
+																	@RequestBody Transaction transaction,
+																	@PathVariable("categoryId") Integer categoryId,
+																	@PathVariable("transactionId") Integer transactionId) {
+		int userId = (Integer) request.getAttribute("userId");
+		transactionService.updateTransaction(userId, categoryId, transactionId, transaction);
+		Map<String, Boolean> map = new HashMap<>();
+		map.put("success", true);
+		return new ResponseEntity<>(map, HttpStatus.OK);
+	}
+}
